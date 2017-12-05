d2d9603bb34a7cb843e8d9d8de239996ffe98f94
philip fraser <pr.fraser3@gmail.com>
2017-12-05 10:48:16 +1000

    added button and click counter

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    clicked: 0
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      clicked: ++prevState.clicked
+    }))
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button 
+          onClick={this.handleClick}>
+          I've been clicked {this.state.clicked} times.
+        </button>
       </div>
     );
   }
